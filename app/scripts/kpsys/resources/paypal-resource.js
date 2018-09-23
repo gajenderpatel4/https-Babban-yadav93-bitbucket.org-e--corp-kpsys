c4deaa30ef1aab421bc5710212af666c5cae0ebf
varnie <varnie29a@mail.ru>
2018-09-23 23:39:52 +0500

    extend PayPal resource extend, add /api/paypal/checkout endpoint

--- app/scripts/kpsys/resources/paypal-resource.js
+++ app/scripts/kpsys/resources/paypal-resource.js
@@ -1,15 +1,28 @@
 'use strict';
 
 angular.module('kpsysApp').factory('PayPalResource', function ($resource) {
-    return $resource('/api/paypal/proceed', {}, {
-        'proceed': {
-            method: 'POST', data: {}, transformResponse: function (data) {
-                try {
-                    return JSON.parse(data);
-                } catch (e) {
-                    return {};
+    return {
+        payPalProceed: $resource('/api/paypal/proceed', {}, {
+            'proceed': {
+                method: 'POST', data: {}, transformResponse: function (data) {
+                    try {
+                        return JSON.parse(data);
+                    } catch (e) {
+                        return {};
+                    }
                 }
             }
-        }
-    });
+        }),
+        paypalCheckout: $resource('/api/paypal/checkout', {}, {
+            'checkout': {
+                method: 'POST', data: {}, transformResponse: function (data) {
+                    try {
+                        return JSON.parse(data);
+                    } catch (e) {
+                        return {};
+                    }
+                }
+            }
+        })
+    };
 });
