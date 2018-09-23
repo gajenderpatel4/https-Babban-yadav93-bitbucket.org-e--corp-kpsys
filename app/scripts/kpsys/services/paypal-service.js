a2b91cbd5edf5a288f6b252093359a2ccab66b87
varnie <varnie29a@mail.ru>
2018-09-23 23:41:23 +0500

    extend PayPal servuce, add checkout

--- app/scripts/kpsys/services/paypal-service.js
+++ app/scripts/kpsys/services/paypal-service.js
@@ -3,7 +3,17 @@
 angular.module('kpsysApp').service('PayPalService', function (PayPalResource, $q) {
     this.proceed = function (p) {
         var defer = $q.defer();
-        PayPalResource.proceed(p, function (result) {
+        PayPalResource.payPalProceed.proceed(p, function (result) {
+            defer.resolve(result);
+        }, function (ex) {
+            defer.reject(ex);
+        });
+        return defer.promise;
+    };
+
+    this.checkout = function (p) {
+        var defer = $q.defer();
+        PayPalResource.paypalCheckout.checkout(p, function (result) {
             defer.resolve(result);
         }, function (ex) {
             defer.reject(ex);
