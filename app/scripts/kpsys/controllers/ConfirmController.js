7be2e63d728e8da6f52cc545ce2995491f3f0735
varnie <varnie29a@mail.ru>
2018-10-05 14:14:42 +0500

    confirm subpage update

--- app/scripts/kpsys/controllers/ConfirmController.js
+++ app/scripts/kpsys/controllers/ConfirmController.js
@@ -63,10 +63,6 @@ angular.module('kpsysApp').controller('ConfirmCtrl', function ($location, $scope
         var paymentId = $location.search().paymentId || "";
         var payerID = $location.search().PayerID || "";
 
-        $scope.guid = guid;
-        $scope.paymentId = paymentId;
-        $scope.payerID = payerID;
-
         $scope.paypalParams = {
             guid: guid,
             paymentId: paymentId,
