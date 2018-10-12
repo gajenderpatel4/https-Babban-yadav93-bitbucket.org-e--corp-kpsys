e4aa391a6ebd8d0f22cddf7bd156fdb5e0484b83
varnie <varnie29a@mail.ru>
2018-10-12 19:21:29 +0500

    show an error in case of client id retrieval failure

--- app/scripts/kpsys/app.js
+++ app/scripts/kpsys/app.js
@@ -60,6 +60,7 @@ kpsysApp.controller('AppController', function ($scope, $rootScope, $window, $loc
     });
 
     $scope.init = function () {
+        $rootScope.unableToDetermineClientForHostname = false;
         var currentClientId = $window.sessionStorage.clientId;
         if (currentClientId === undefined) {
             var hostname = $location.host();
@@ -70,6 +71,7 @@ kpsysApp.controller('AppController', function ($scope, $rootScope, $window, $loc
                     console.log(response);
                 }, function (ex) {
                     console.log(ex);
+                    $rootScope.unableToDetermineClientForHostname = true;
                 });
         }
     };
