29eb43d0768283e9316b79486cc65d0de3f056d5
varnie <varnie29a@mail.ru>
2018-10-20 16:24:54 +0500

    enable SQL migrations

--- kpsys/src/main/resources/db/migration/V09__update_guru_anprgate_device.sql
+++
@@ -1,9 +0,0 @@
--- Adds columns related to camera addresses
--- Created by dkhvatov on 2017-02-26
-
-ALTER TABLE guru_anprgate_device
-    ADD CAM0MASK VARCHAR(64);
-ALTER TABLE guru_anprgate_device
-    ADD CAM1MASK VARCHAR(64);
-ALTER TABLE guru_anprgate_device
-    ADD IPPPADDR VARCHAR(32); -- IP ADDRESS OF THE DEVICE
--- kpsys/src/main/resources/db/migration/V10__add_tckt_role.sql
+++
@@ -1,4 +0,0 @@
--- Adds TCKT (parking fine) role to default user
--- Created by dkhvatov on 2017-05-14
-
-INSERT INTO kp_role_to_user (ROLEIDNO, USERIDNO, ADITPARM, CLNTIDNO) VALUES (21, 401, NULL, 24);
