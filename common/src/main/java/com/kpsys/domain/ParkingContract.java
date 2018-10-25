3170f91df04856780a2f6f1a589c192f4a820aa3
varnie <varnie29a@mail.ru>
2018-10-25 12:40:20 +0500

    entity corrections

--- common/src/main/java/com/kpsys/domain/ParkingContract.java
+++ common/src/main/java/com/kpsys/domain/ParkingContract.java
@@ -1,5 +1,6 @@
 package com.kpsys.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,21 +29,23 @@ public class ParkingContract {
     @Column(name = "zone_id", nullable = false)
     private Long zoneId;
 
-    @Column(name = "client_id", nullable = false)
-    private Long clientId;
+    @JoinColumn(name = "client_id")
+    @ManyToOne(targetEntity = Client.class, cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JsonIgnore
+    private Client client;
 
     @Column(name = "name", length = 35, nullable = false)
     private String name;
 
-    @Column(name = "max_item_count", length = 10)
+    @Column(name = "max_item_count", precision = 10)
     private Long maxItemCount;
 
     @Column(name = "description", length = 200)
     private String description;
 
-    @Column(name = "contract_type", length = 10)
+    @Column(name = "contract_type", precision = 10)
     private Long contractType;
 
-    @Column(name = "max_utilization_count", length = 10)
+    @Column(name = "max_utilization_count", precision = 10)
     private Long maxUtilizationCount;
 }
