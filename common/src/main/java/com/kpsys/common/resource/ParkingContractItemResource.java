0b1106c1f73196509f6e8e900de8858c85fe51fc
varnie <varnie29a@mail.ru>
2018-11-03 22:16:22 +0500

    edit ParkingContract and ParkingContractItem objects

--- common/src/main/java/com/kpsys/common/resource/ParkingContractItemResource.java
+++ common/src/main/java/com/kpsys/common/resource/ParkingContractItemResource.java
@@ -1,8 +1,11 @@
 package com.kpsys.common.resource;
 
+import com.codahale.metrics.annotation.Timed;
 import com.google.inject.Inject;
 import com.kpsys.common.dao.ParkingContractItemDao;
+import com.kpsys.common.dto.BaseResponse;
 import com.kpsys.common.dto.EntityResponse;
+import com.kpsys.common.exceptions.KpsysException;
 import com.kpsys.domain.ParkingContractItem;
 import com.kpsys.domain.User;
 import io.dropwizard.auth.Auth;
@@ -13,7 +16,11 @@ import org.slf4j.LoggerFactory;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
+import java.util.Optional;
+
+import static com.kpsys.common.dto.BaseResponse.ok;
 
 @Path("/parkingcontractitem")
 public class ParkingContractItemResource {
@@ -26,6 +33,7 @@ public class ParkingContractItemResource {
     @GET
     @Path("/findByParkingContract/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Timed
     @UnitOfWork
     public EntityResponse<List<ParkingContractItem>> findByParkingContract(@Auth User principal, @PathParam("id") Integer parkingContractId) {
         Integer userId = principal.getUserId();
@@ -35,8 +43,27 @@ public class ParkingContractItemResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Timed
     @UnitOfWork
     public EntityResponse<ParkingContractItem> saveParkingContractItem(@Auth User principal, @Valid ParkingContractItem parkingContractItem) {
         return EntityResponse.of(parkingContractItemDao.save(parkingContractItem));
+    }
+
+    @Path("{id}")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Timed
+    @UnitOfWork
+    public BaseResponse deleteUser(@Auth User principal, @PathParam("id") Integer parkingContractItemId) {
+        Integer userId = principal.getUserId();
+        Optional<ParkingContractItem> parkingContractItemOptional = parkingContractItemDao.getByUserIdAndId(userId, parkingContractItemId);
+
+        ParkingContractItem parkingContractItem = parkingContractItemOptional.orElseThrow(() -> {
+            LOGGER.error("Unable to find parking contract item with id: " + parkingContractItemId);
+            return new KpsysException("Unable to find parking contract item with id: " + parkingContractItemId, Response.Status.NOT_FOUND);
+        });
+
+        parkingContractItemDao.delete(parkingContractItem);
+        return ok();
     }
 }
--- common/src/main/java/com/kpsys/common/resource/ParkingContractResource.java
+++ common/src/main/java/com/kpsys/common/resource/ParkingContractResource.java
@@ -1,13 +1,16 @@
 package com.kpsys.common.resource;
 
+import com.codahale.metrics.annotation.Timed;
 import com.google.inject.Inject;
 import com.kpsys.common.dao.ParkingContractDao;
+import com.kpsys.common.dto.BaseResponse;
 import com.kpsys.common.dto.EntityResponse;
 import com.kpsys.common.exceptions.KpsysException;
 import com.kpsys.domain.ParkingContract;
 import com.kpsys.domain.User;
 import io.dropwizard.auth.Auth;
 import io.dropwizard.hibernate.UnitOfWork;
+import org.hibernate.exception.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +19,9 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Optional;
+
+import static com.kpsys.common.dto.BaseResponse.ok;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
 @Path("/parkingcontract")
 public class ParkingContractResource {
@@ -28,6 +34,7 @@ public class ParkingContractResource {
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Timed
     @UnitOfWork
     public EntityResponse<ParkingContract> findParkingContractById(@Auth User principal, @PathParam("id") Integer id) {
         Integer userId = principal.getUserId();
@@ -42,8 +49,36 @@ public class ParkingContractResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
+    @Timed
     @UnitOfWork
     public EntityResponse<ParkingContract> saveParkingContract(@Auth User principal, @Valid ParkingContract parkingContract) {
         return EntityResponse.of(parkingContractDao.save(parkingContract));
+    }
+
+    @Path("{id}")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Timed
+    @UnitOfWork
+    public BaseResponse delete(@Auth User principal, @PathParam("id") Integer parkingContractId) {
+        Integer userId = principal.getUserId();
+        Optional<ParkingContract> parkingContractOptional = parkingContractDao.getParkingContractByIdAndUserId(parkingContractId, userId);
+        ParkingContract parkingContract = parkingContractOptional.orElseThrow(() -> {
+            LOGGER.error("Unable to find parking contract with id: " + parkingContractId);
+            return new KpsysException("Unable to find parking contract with id: " + parkingContractId, Response.Status.NOT_FOUND);
+        });
+
+        try {
+            parkingContractDao.delete(parkingContract);
+        } catch (ConstraintViolationException e) {
+            Throwable cause = e.getCause();
+            if (cause.getMessage().contains("The DELETE statement conflicted with the REFERENCE constraint \"fk_parking_contract_contract\"")) {
+                throw new KpsysException("This Parking Contract has connected with Parking Contract Role and cannot be deleted.", e, BAD_REQUEST);
+            } else {
+                throw e;
+            }
+        }
+
+        return ok();
     }
 }
