eb9bffd06c8cf328b7b460a49ab63010d62f10e3
varnie <varnie29a@mail.ru>
2018-11-08 12:56:51 +0500

    code cleanup

--- common/src/main/java/com/kpsys/domain/ParkingContract.java
+++ common/src/main/java/com/kpsys/domain/ParkingContract.java
@@ -13,7 +13,6 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 @JsonInclude(NON_NULL)
 @Data
 @NamedQueries({
-    //@NamedQuery(name = "getParkingContractsByUserId", query = "SELECT DISTINCT pc FROM ParkingContract pc JOIN ParkingContractRole pcr ON pcr.parkingContractId = pc.id WHERE pcr.userId = :user_id"),
     @NamedQuery(name = "getByIdAndUserId", query = "SELECT pc FROM ParkingContract pc JOIN ParkingContractRole pcr ON pcr.parkingContractId = pc.id WHERE pcr.userId = :user_id AND pcr.parkingContractId = :parking_contract_id AND pcr.role_type = 'edit'")
 })
 public class ParkingContract {
