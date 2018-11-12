c06bfecc288063c3c560eead045aed0b0233f760
varnie <varnie29a@mail.ru>
2018-11-12 10:42:52 +0500

    fix sql query

--- common/src/main/java/com/kpsys/domain/ParkingContractItem.java
+++ common/src/main/java/com/kpsys/domain/ParkingContractItem.java
@@ -13,14 +13,14 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 @JsonInclude(NON_NULL)
 @Data
 @NamedQueries({
-    @NamedQuery(name = "getParkingContractItemsByUserIdAndParkingContractId", query = "SELECT DISTINCT pci FROM ParkingContractItem pci " +
+    @NamedQuery(name = "getParkingContractItemsByUserIdAndParkingContractId", query = "SELECT pci FROM ParkingContractItem pci " +
         "JOIN ParkingContract pc ON pc.id = pci.parkingContract.id " +
         "JOIN ParkingContractRole pcr ON pcr.parkingContractId = pc.id WHERE pcr.userId = :user_id AND pcr.role_type = 'edit' AND pci.parkingContract.id = :parking_contract_id"),
     @NamedQuery(name = "getParkingContractItemByUserIdAndId", query = "SELECT pci FROM ParkingContractItem pci " +
-        "WHERE EXISTS (SELECT pc FROM ParkingContract pc " +
+        "WHERE pci.id = :id AND " +
+        "EXISTS (SELECT pc FROM ParkingContract pc " +
         "JOIN ParkingContractRole pcr ON pcr.parkingContractId = pc.id " +
-        "WHERE pcr.userId = :user_id and pcr.role_type = 'edit' and pc.id = pci.parkingContract.id) " +
-        "AND pci.id = :id")})
+        "WHERE pcr.userId = :user_id and pcr.role_type = 'edit' and pc.id = pci.parkingContract.id)")})
 public class ParkingContractItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
