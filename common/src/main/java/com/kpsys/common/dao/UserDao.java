595ab117363e8adbacf42c86af4247279bb19580
varnie <varnie29a@mail.ru>
2018-11-23 14:52:37 +0500

    old redundant methods removal

--- common/src/main/java/com/kpsys/common/dao/UserDao.java
+++ common/src/main/java/com/kpsys/common/dao/UserDao.java
@@ -3,56 +3,13 @@ package com.kpsys.common.dao;
 import com.google.inject.Inject;
 import com.kpsys.common.multitenancy.TenantResolver;
 import com.kpsys.domain.User;
-import com.kpsys.domain.enums.OrderType;
-import com.kpsys.domain.enums.UserType;
-import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Disjunction;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.criterion.SimpleExpression;
-
-import java.util.List;
-
-import static org.hibernate.criterion.MatchMode.ANYWHERE;
 
 public class UserDao extends MainClientAwareDao<User> {
 
     @Inject
     public UserDao(SessionFactory sessionFactory, TenantResolver tenantResolver) {
         super(sessionFactory, tenantResolver);
-    }
-
-    public List<User> getUsers(int start, int max, String orderBy, OrderType orderType) {
-        return list(criteria()
-            .addOrder(orderType.getOrder(orderBy))
-            .setFirstResult(start)
-            .setMaxResults(max));
-    }
-
-    public List<User> getAllUsers() {
-        return list(criteria());
-    }
-
-    public int countUsers() {
-        return ((Number) criteria()
-            .setProjection(Projections.rowCount())
-            .uniqueResult())
-            .intValue();
-    }
-
-    public List<User> getFilteredUsers(int start, int max, String orderBy, OrderType orderType, UserType userType, String searchQuery) {
-        Criteria criteria = getFilteredUsersCriteria(userType, searchQuery);
-        criteria.addOrder(orderType.getOrder(orderBy))
-            .setFirstResult(start)
-            .setMaxResults(max);
-        return list(criteria);
-    }
-
-    public int getFilteredUsersCount(UserType userType, String searchQuery) {
-        Criteria criteria = getFilteredUsersCriteria(userType, searchQuery);
-        criteria.setProjection(Projections.rowCount());
-        return ((Number) criteria.uniqueResult()).intValue();
     }
 
     public User getUser(int userId) {
@@ -64,23 +21,4 @@ public class UserDao extends MainClientAwareDao<User> {
         currentSession().flush();
         return savedUser;
     }
-
-    public void delete(User user) {
-        currentSession().delete(user);
-        currentSession().flush();
-    }
-
-    private Criteria getFilteredUsersCriteria(UserType userType, String searchQuery) {
-        Criteria criteria = criteria()
-            .add(Restrictions.eq("userType", userType));
-        if (!searchQuery.isEmpty()) {
-            SimpleExpression username = Restrictions.like("username", searchQuery, ANYWHERE);
-            SimpleExpression email = Restrictions.like("email", searchQuery, ANYWHERE);
-            SimpleExpression phone = Restrictions.like("phone", searchQuery, ANYWHERE);
-            Disjunction search = Restrictions.disjunction(username, email, phone);
-            criteria.add(search);
-        }
-        return criteria;
-    }
-
 }
