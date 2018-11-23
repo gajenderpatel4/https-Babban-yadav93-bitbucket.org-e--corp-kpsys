a8c1ffb5fd24b8972cdaff176893935c7a3f852c
varnie <varnie29a@mail.ru>
2018-11-23 15:45:59 +0500

    unneeded commented code removal

--- app/scripts/kpsys/resources/users-resource.js
+++ app/scripts/kpsys/resources/users-resource.js
@@ -1,24 +1,7 @@
 'use strict';
 
-angular.module('kpsysApp').factory('UsersResource', function ($resource, PaginationConstants) {
+angular.module('kpsysApp').factory('UsersResource', function ($resource) {
     return {
-        /*getUsersFiltered : $resource('/api/dashboard/users/filtered', {}, {}),
-        getUsers : $resource('/api/dashboard/users', {}, {
-            get : {
-                method: 'GET', isArray: true, transformResponse: function (data, headers) {
-                   var jsonData = JSON.parse(data);
-                   headers()[PaginationConstants.PAGINATION_COUNT] = jsonData.count;
-                   headers()[PaginationConstants.PAGINATION_START] = jsonData.start;
-                   headers()[PaginationConstants.PAGINATION_LIMIT] = jsonData.limit;
-                   return jsonData.list;
-                }
-            }
-        }),
-        createUser : $resource('/api/dashboard/users', {}, {
-            create : {
-                method:'POST', data: { user: {} }
-            }
-        }),*/
         user: $resource('/api/users', {}, {
             get: {
                 method: "POST"
