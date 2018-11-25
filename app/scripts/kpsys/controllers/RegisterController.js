c2ac079c06411994396a2294dc1a2118786d45ea
varnie <varnie29a@mail.ru>
2018-11-25 22:37:26 +0500

    prevent multiple forms submissions; debug code removal

--- app/scripts/kpsys/controllers/RegisterController.js
+++ app/scripts/kpsys/controllers/RegisterController.js
@@ -1,5 +1,4 @@
 'use strict';
-//TODO:
 
 angular.module('kpsysApp').controller('RegisterCtrl', function ($scope, $rootScope, $window, $state, $translate, RegisterService, AUTH_EVENTS, InternationalizationConstants, Session) {
 
@@ -13,36 +12,59 @@ angular.module('kpsysApp').controller('RegisterCtrl', function ($scope, $rootSco
     $scope.registerError = null;
 
     $scope.register = function (registerData) {
+        if ($scope.loading) {
+            return;
+        }
+
         $scope.submitted[0] = true;
         if ($scope.form.$invalid) {
             return;
         }
+
+        $scope.loading = true;
+        $scope.registerError = false;
+
         registerData.phone = $scope.country.selected.phoneCode + registerData.phoneNumber;
         RegisterService.register(registerData).then(function (res) {
             $window.localStorage.phone = res.data.phone;
             $rootScope.$broadcast(AUTH_EVENTS.registerSuccess);
+            $scope.loading = false;
         }, function (res) {
-            $scope.registerError = res.data;
+            $scope.registerError = $rootScope.getErrorMessage(res);
+            $scope.loading = false;
         });
     };
 
     $scope.confirmRegistration = function (confirmCode) {
+        if ($scope.loading) {
+            return;
+        }
+
         $scope.submitted[1] = true;
         if (confirmCode == null) {
             return;
         }
+
+        $scope.loading = true;
+
         RegisterService.confirmRegistration({
             phone: $window.localStorage.phone,
             code: confirmCode
         }).then(function (_) {
             $scope.invalidConfirmCode = false;
+            $scope.loading = false;
         }, function () {
             $scope.invalidConfirmCode = true;
             $rootScope.$broadcast(AUTH_EVENTS.confirmationFailed);
+            $scope.loading = false;
         });
     };
 
     $scope.completeRegistration = function (password, repeatPassword) {
+        if ($scope.loading) {
+            return;
+        }
+
         $scope.submitted[2] = true;
         if (password !== repeatPassword) {
             return;
@@ -50,16 +72,20 @@ angular.module('kpsysApp').controller('RegisterCtrl', function ($scope, $rootSco
         if ($scope.form.$invalid) {
             return;
         }
+
+        $scope.loading = true;
+
         RegisterService.completeRegistration({
             token: Session.accessToken(),
             password: password
         }).then(function (res) {
+            $scope.loading = false;
         }, function () {
-        })
+            $scope.loading = false;
+        });
     };
 
     $scope.init = function () {
-
         $scope.countries = InternationalizationConstants.supportedLanguages.map(function (lang) {
             return {
                 code: lang,
@@ -67,7 +93,6 @@ angular.module('kpsysApp').controller('RegisterCtrl', function ($scope, $rootSco
                 phoneCode: InternationalizationConstants.phoneCodes[lang]
             };
         });
-        console.log($scope.countries);
         var language = $window.localStorage.lang || $translate.preferredLanguage();
         $translate.preferredLanguage(language);
         $scope.country.selected = {
@@ -75,6 +100,7 @@ angular.module('kpsysApp').controller('RegisterCtrl', function ($scope, $rootSco
             name: InternationalizationConstants.countries[language],
             phoneCode: InternationalizationConstants.phoneCodes[language]
         };
-    }
 
+        $scope.loading = false;
+    }
 });
